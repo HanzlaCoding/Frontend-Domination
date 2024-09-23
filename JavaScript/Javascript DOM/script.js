67d21bab29373993ebc35d859a594e371e3f2bfb
Hanzla Dev <you@example.com>
2024-09-24 01:06:04 +0500

    Done

--- JavaScript/Javascript DOM/script.js
+++ JavaScript/Javascript DOM/script.js
@@ -1,12 +1,13 @@
 // DOM (Document Object Model)
 var h1 = document.querySelector('h1');
 var btn = document.querySelector('button');
-var div = document.createElement('div');
+// var div = document.createElement('div');
 
 
-div.textContent = 'Hello';
-document.querySelector('body').append(div);
-div.style.color = 'red';
-div.style.fontSize = '150px';
-div.style.fontFamily = 'Gilroy';
+// div.textContent = 'Hello';
+// document.querySelector('body').append(div);
+// div.style.color = 'red';
+// div.style.fontSize = '150px';
+// div.style.fontFamily = 'Gilroy';
 
+h1.classList.add('h1Makeup')
